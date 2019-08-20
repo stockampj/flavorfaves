512e3f1da625bc6141cff4a46cc299111e81eb8f
Joel Stockamp and sofia giordano <joel@>
2019-08-20 09:37:07 -0700

    add user input form, array loop

--- scripts.js
+++ scripts.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
-  var flavors = ['vanilla', 'chocolate', 'strawberry']
+  $('form.flaveinput').submit(function(event) {
+    event.preventDefault();
 
-  var outputArray = []
-    outputArray.push(flavors[0], flavors[1], flavors[2]);
+
+    var flavors = [$('#ui1').val(), $('#ui2').val(), $('#ui3').val()]
+    console.log(flavors);
+
+    var outputArray = []
+    // outputArray.push(flavors[0], flavors[1], flavors[2]);
+
+    flavors.forEach(function(flavor) {
+      // outputArray.push(flavor);
+      $('#flavelist').append('<li>' + flavor + '</li>')
+    });
+
+
+
+
+
 
     // the id on the html and the js tells the output where to go
-    $('li#output1').text(outputArray[0]);
-    $('li#output2').text(outputArray[1]);
-    $('li#output3').text(outputArray[2]);
+    // $('li#output1').text(outputArray[0]);
+    // $('li#output2').text(outputArray[1]);
+    // $('li#output3').text(outputArray[2]);
 
-
+  });
 });
