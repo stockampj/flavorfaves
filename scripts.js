985bdbbac72d7ede954863b3200ddc7fc86a9ed0
Joel Stockamp and sofia giordano <sofia@>
2019-08-20 09:51:47 -0700

    make a for loop

--- scripts.js
+++ scripts.js
@@ -3,26 +3,20 @@ $(document).ready(function() {
     event.preventDefault();
 
 
-    var flavors = [$('#ui1').val(), $('#ui2').val(), $('#ui3').val()]
-    console.log(flavors);
-
-    var outputArray = []
-    // outputArray.push(flavors[0], flavors[1], flavors[2]);
-
-    flavors.forEach(function(flavor) {
-      // outputArray.push(flavor);
-      $('#flavelist').append('<li>' + flavor + '</li>')
-    });
+    var flavors = [
+      $('#ui1').val(),
+      $('#ui2').val(),
+      $('#ui3').val()
+    ]
 
 
+    // flavors.forEach(function(flavor) {
+    //   $('#flavelist').append('<li>' + flavor + '</li>')
+    // });
 
-
-
-
-    // the id on the html and the js tells the output where to go
-    // $('li#output1').text(outputArray[0]);
-    // $('li#output2').text(outputArray[1]);
-    // $('li#output3').text(outputArray[2]);
-
+    for (var index = 0; index < flavors.length; index += 1) {
+      var flavor = flavors[index];
+      $('#flavelist').append('<li>' + flavor + '</li>');
+    }
   });
 });
